8ea8433d1f90069fbd619eb607ff6e29b9f77a2c
TiagoCampos <droiddzn68@gmail.com>
2026-04-21 00:30:47 -0300

    Melhorando detalhes na pagina

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,25 +12,48 @@ import {
 import { Menu } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 export default function Header() {
+
+    const [user, setUser] = useState(true);
+
+    function login() {
+        setUser(true);
+    }
+
+    function logout() {
+        setUser(false);
+    }
+
     return (
         <header className="flex justify-around bg-black w-full h-15 items-center text-white">
             <Link className="text-3xl" to="/">Pipoca<span className="text-orange-500">Flix</span></Link>
-            <Link className="hidden md:flex" to="/favoritos">Meus filmes</Link>
-            <Sheet>
-                <SheetTrigger className="md:hidden"><Menu /></SheetTrigger>
-                <SheetContent>
-                    <SheetHeader className="bg-black text-white">
-                        <SheetTitle><Link className="text-2xl text-white" to="/">Pipoca<span className="text-orange-500">Flix</span></Link></SheetTitle>
-                        <SheetDescription>Navegação</SheetDescription>
-                    </SheetHeader>
 
-                    <div className="flex justify-center flex-col items-center w-full mt-10">
-                        {/* <Link className="bg-orange-500 text-white text-center p-2 w-[95%] rounded-lg" to="/favoritos">Meus filmes</Link> */}
+            {user ? (
+                <div className="flex items-center gap-5">
+                    <Link className="hidden md:flex" to="/favoritos">Meus filmes</Link>
+                    <Button onClick={logout} className="hidden md:flex">Sair</Button>
+                    <Sheet>
+                        <SheetTrigger className="md:hidden"><Menu /></SheetTrigger>
+                        <SheetContent>
+                            <SheetHeader className="bg-black text-white">
+                                <SheetTitle><Link className="text-2xl text-white" to="/">Pipoca<span className="text-orange-500">Flix</span></Link></SheetTitle>
+                                <SheetDescription>Navegação</SheetDescription>
+                            </SheetHeader>
+
+                            <div className="flex justify-center flex-col items-center  w-full mt-10">
+                                <Button>Sair</Button>
+                            </div>
+                        </SheetContent>
+                    </Sheet>
+                </div>
+            ) : (
+                <>
+                    <div className="flex gap-10 items-center">
 
                         <Sheet>
-                            <SheetTrigger className="bg-orange-500 p-2 w-[90%] cursor-pointer duration-200 hover:bg-orange-300 text-white rounded-2xl">Entrar</SheetTrigger>
+                            <SheetTrigger className="bg-orange-500 hidden md:flex p-2 cursor-pointer duration-200 hover:bg-orange-300 text-white rounded-2xl">Entrar</SheetTrigger>
                             <SheetContent>
                                 <SheetClose side="left">Fechar</SheetClose>
                                 <SheetHeader className="flex gap-10">
@@ -40,13 +63,60 @@ export default function Header() {
                                     </div>
                                     <Input placeholder="Email" />
                                     <Input placeholder="Senha" />
+                                    <Button onClick={login} className="bg-orange-500">Fazer login</Button>
+                                </SheetHeader>
+                            </SheetContent>
+                        </Sheet>
+
+                        <Sheet>
+                            <SheetTrigger className="text-orange-500 hidden md:flex p-2 cursor-pointer  rounded-2xl">Criar conta</SheetTrigger>
+                            <SheetContent>
+                                <SheetClose side="left">Fechar</SheetClose>
+                                <SheetHeader className="flex gap-10">
+                                    <div>
+                                        <SheetTitle>Criar conta</SheetTitle>
+                                        <SheetDescription>Faça login na sua conta</SheetDescription>
+                                    </div>
+                                    <Input placeholder="Email" />
+                                    <Input placeholder="Senha" />
                                     <Button className="bg-orange-500">Fazer login</Button>
                                 </SheetHeader>
                             </SheetContent>
                         </Sheet>
                     </div>
-                </SheetContent>
-            </Sheet>
+
+
+                    <Sheet>
+                        <SheetTrigger className="md:hidden"><Menu /></SheetTrigger>
+                        <SheetContent>
+                            <SheetHeader className="bg-black text-white">
+                                <SheetTitle><Link className="text-2xl text-white" to="/">Pipoca<span className="text-orange-500">Flix</span></Link></SheetTitle>
+                                <SheetDescription>Navegação</SheetDescription>
+                            </SheetHeader>
+
+                            <div className="flex justify-center flex-col items-center w-full mt-10">
+                                {/* <Link className="bg-orange-500 text-white text-center p-2 w-[95%] rounded-lg" to="/favoritos">Meus filmes</Link> */}
+
+                                <Sheet>
+                                    <SheetTrigger className="bg-orange-500 p-2 w-[90%] cursor-pointer duration-200 hover:bg-orange-300 text-white rounded-2xl">Entrar</SheetTrigger>
+                                    <SheetContent>
+                                        <SheetClose side="left">Fechar</SheetClose>
+                                        <SheetHeader className="flex gap-10">
+                                            <div>
+                                                <SheetTitle>Entrar</SheetTitle>
+                                                <SheetDescription>Faça login na sua conta</SheetDescription>
+                                            </div>
+                                            <Input placeholder="Email" />
+                                            <Input placeholder="Senha" />
+                                            <Button onClick={login} className="bg-orange-500">Fazer login</Button>
+                                        </SheetHeader>
+                                    </SheetContent>
+                                </Sheet>
+                            </div>
+                        </SheetContent>
+                    </Sheet>
+                </>
+            )}
 
 
         </header>
